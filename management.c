f6fcb3c18d200c6b8282c744e36d4fde1e0195e8
Chan2ie <dlcksgml17@u.sogang.ac.kr>
2017-12-13 10:07:26 -0800

    Header Seperated

--- management.c
+++ management.c
@@ -1,119 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ncurses.h>
-#include <string.h>
-#include <malloc.h>
-#include <time.h>
-
-#define MENU_ASSIGN '1'
-#define MENU_CGPA '2'
-#define MENU_CHANGE '3'
-#define MENU_LOGOUT '4'
-#define MENU_LOGIN '1'
-#define MENU_NEW '2'
-#define MENU_DEL '3'
-#define MENU_TEMP '4'
-#define MENU_QUIT '5'
-#define CGPA_ADD '1'
-#define CGPA_VIEW '2'
-#define CGPA_QUIT '3'
-#define LOGIN_FAILED 7
-#define LOGIN_SCCUESS 1
-#define EXIT 5
-#define LOGOUT 4
-#define INVAILD_CHOICE 6
-#define NEW_ACCOUNT_FAIL 21
-#define NEW_ACCOUNT_SUCCESS 2
-#define DELETE_ACCOUNT_FAIL_INVALID 31
-#define DELETE_ACCOUNT_FAIL_PASSWORD 32
-#define DELETE_ACCOUNT_SUCCESS 3
-#define MAIN_MENU 23
-#define CHANGE_PASSWORD_SUCCESS 131
-#define CHANGE_PASSWORD_FAIL 132
-
-char Curr_Num[9];
-char Curr_Pass[16];
-char New_Num[9];
-char New_Pass[16];
-int Curr_year_index;
-int Curr_num_index;
-
-typedef struct CHILD1{
-  char name[100];
-  char describe[100];
-  char professor[100];
-  int date[2];
-  struct CHILD1 *link;
-}ASSIGN;
-
-typedef struct CHILD2{
-  int semester;
-  float score;
-  struct CHILD2 *link;
-}CGPA;
-
-typedef struct SUB_SUB_HEAD{
-  char number[5];
-  char password[16];
-  ASSIGN* Child_A;
-  int Assign_Size;
-  int CGPA_Size;
-  CGPA* Child_C;
-  struct SUB_SUB_HEAD *link;
-}STUDENT;
-
-typedef struct SUB_HEAD{
-  char year[5];
-  int Num_Size;
-  STUDENT* ST_NUM;
-}YEAR;
-
-typedef struct HEAD{
-  int Year_Size;
-  YEAR* ST_YEAR;
-}TREE_HEAD;
-
-int Login_Year;
-int Login_Num;
-int Year_Count=8;
-int Student_Count;
-int Assign_Count;
-int CGPA_Count;
-
-TREE_HEAD* TOP;
-void Load_Data ();
-void Save_Data ();
-char menu ();
-char cgpa_menu ();
-void Add_GPA ();
-void Cor_GPA (STUDENT *login_st, char semester,float gpa);
-void Create_Struct ();
-void Print_Assign ();
-void Print_CGPA ();
-void Print_CGPA_Graph ();
-int New_Account ();
-int Delete_Account ();
-int Change_Password ();
-void rank ();
-void Sort_Assign ();
-void Add_Assign ();
-void Delete_Assign ();
-int login ();
-void Temp_Password ();
-char Account_Manage ();
-void read_data (FILE * fpoint);
-void create_student ();
-void create_assign (); 
-void create_cgpa (CGPA *new, int semester, float score);
-int search_year ();
-STUDENT*  search_num ();
-int run_account_manage ();
-int run_menu ();
-int run_cgpa_menu ();
-int run_assign_menu ();
-void free_student ();
-void free_assign ();
-void free_cgpa ();
+#include "management.h"
 
 int main(){
     int exit=0;
@@ -343,7 +228,7 @@ void Save_Data(){
     fclose(fpoint);
 }
 
-int search_year (char year[5]) {
+int search_year (char year[]) {
     int year_num;
 
     for(year_num = 0; year_num < TOP->Year_Size; year_num++){
@@ -355,7 +240,7 @@ int search_year (char year[5]) {
     return year_num;
 }
 
-STUDENT* search_num (char number[5], int year_num) {
+STUDENT* search_num (char number[], int year_num) {
     STUDENT *sptr;
     sptr = TOP->ST_YEAR[year_num].ST_NUM;
 
@@ -389,7 +274,8 @@ void read_data (FILE * fpoint) {
     char password[16], year[5], number[5], name[100], describe[100], professor[100];
 
     while(1){
-        if( fscanf(fpoint, "%4s%4s / %s / %d / %d\n", year, number, password, &a_size, &c_size) != 5 ){
+        if( fscanf(fpoint, "%4s%4s / %s / %d / %d\n", 
+            year, number, password, &a_size, &c_size) != 5 ){
             break;
         }
         else{
@@ -740,17 +626,8 @@ void Print_CGPA(STUDENT *login_st) {
     }
     
     printw("------------------------------\n\n");
-//    printw("<CGPA Management by Graph for %s>\n", Curr_Num);
-//    printw("(x-axis : semester,  y-axis : score)\n");
-//    Print_CGPA_Graph();
     
     getch();
-}
-
-void Print_CGPA_Graph () {
- 	/*
-		To do...(Additional)
-	*/
 }
 
 void Sort_Assign (STUDENT *login_st) {
@@ -784,8 +661,10 @@ void Sort_Assign (STUDENT *login_st) {
 
             if(j == (i - 1)) ;
             else{
-                if(100*check->date[0] + check->date[1] >= 100*bef->date[0] + bef->date[1]
-                        && 100*check->date[0] + check->date[1] < 100*bef->link->date[0] + bef->link->date[1]){
+                if(100*check->date[0] + check->date[1] 
+                    >= 100*bef->date[0] + bef->date[1]
+                    && 100*check->date[0] + check->date[1] 
+                    < 100*bef->link->date[0] + bef->link->date[1]){
                     af = bef->link;
                     bef->link = check;
                     check_bf->link = check->link;
--- management.h
+++ management.h
@@ -19,6 +19,19 @@
 #define CGPA_ADD '1'
 #define CGPA_VIEW '2'
 #define CGPA_QUIT '3'
+#define LOGIN_FAILED 7
+#define LOGIN_SCCUESS 1
+#define EXIT 5
+#define LOGOUT 4
+#define INVAILD_CHOICE 6
+#define NEW_ACCOUNT_FAIL 21
+#define NEW_ACCOUNT_SUCCESS 2
+#define DELETE_ACCOUNT_FAIL_INVALID 31
+#define DELETE_ACCOUNT_FAIL_PASSWORD 32
+#define DELETE_ACCOUNT_SUCCESS 3
+#define MAIN_MENU 23
+#define CHANGE_PASSWORD_SUCCESS 131
+#define CHANGE_PASSWORD_FAIL 132
 
 char Curr_Num[9];
 char Curr_Pass[16];
@@ -26,69 +39,79 @@ char New_Num[9];
 char New_Pass[16];
 int Curr_year_index;
 int Curr_num_index;
-typedef struct CHILD1
-{
+
+typedef struct CHILD1{
   char name[100];
   char describe[100];
   char professor[100];
   int date[2];
+  struct CHILD1 *link;
 }ASSIGN;
 
-typedef struct CHILD2
-{
+typedef struct CHILD2{
   int semester;
   float score;
+  struct CHILD2 *link;
 }CGPA;
-typedef struct SUB_SUB_HEAD
-{
+
+typedef struct SUB_SUB_HEAD{
   char number[5];
   char password[16];
   ASSIGN* Child_A;
   int Assign_Size;
   int CGPA_Size;
   CGPA* Child_C;
+  struct SUB_SUB_HEAD *link;
 }STUDENT;
-typedef struct SUB_HEAD
-{
+
+typedef struct SUB_HEAD{
   char year[5];
   int Num_Size;
   STUDENT* ST_NUM;
 }YEAR;
 
-typedef struct HEAD
-{
+typedef struct HEAD{
   int Year_Size;
   YEAR* ST_YEAR;
 }TREE_HEAD;
+
 int Login_Year;
 int Login_Num;
 int Year_Count=8;
 int Student_Count;
 int Assign_Count;
 int CGPA_Count;
+
 TREE_HEAD* TOP;
-void Load_Data();
-void Save_Data();
-char menu();
-char cgpa_menu();
-void Add_GPA();
-void Del_GPA();
-void Cor_GPA(char semester,float gpa);
-void Create_Struct();
-void Search_Assign();
-void Search_CGPA();
-void Print_Assign(int);
-void Print_CGPA();
-void Print_CGPA_Graph();
-void New_Account();
-void Delete_Account();
-void Change_Password();
-void rank();
-void Sort_Assign();
-void Add_Assign();
-void Delete_Assign();
-void Temp_Password();
-int login();
-void Temp_Password();
-char Account_Manage();
+void Load_Data ();
+void Save_Data ();
+char menu ();
+char cgpa_menu ();
+void Add_GPA ();
+void Cor_GPA (STUDENT *login_st, char semester,float gpa);
+void Create_Struct ();
+void Print_Assign ();
+void Print_CGPA ();
+int New_Account ();
+int Delete_Account ();
+int Change_Password ();
+void Sort_Assign ();
+void Add_Assign ();
+void Delete_Assign ();
+int login ();
+void Temp_Password ();
+char Account_Manage ();
+void read_data (FILE * fpoint);
+void create_student ();
+void create_assign (); 
+void create_cgpa (CGPA *new, int semester, float score);
+int search_year ();
+STUDENT*  search_num ();
+int run_account_manage ();
+int run_menu ();
+int run_cgpa_menu ();
+int run_assign_menu ();
+void free_student ();
+void free_assign ();
+void free_cgpa ();
 #endif
